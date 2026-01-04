0bc318519e7331458c357550b111908f1c38c78d
Michael Wilburn <michael-mw@hotmail.com>
2026-01-04 16:59:17 -0300

    Draw Player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import constants
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -13,13 +14,15 @@ def main():
     dt = 0
 
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000 
     
